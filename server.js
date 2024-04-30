3f0cea84e352e12b8b8c75bc555bd729f952fb2a
icedjello <revues.counter0x@icloud.com>
2024-04-30 11:51:47 +0100

    add some gets

--- server.js
+++ server.js
@@ -3,8 +3,53 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3500;
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "./public")));
+
+app.get("^/$|/index(.html)?", (_, res) => {
+  // res.sendFile("./views/index.html", { root: __dirname });
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.get("/new-page(.html)?", (_, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
+});
+
+app.get("/old-page(.html)?", (_, res) => {
+  res.redirect(301, "/new-page.html"); // 302 default
+});
+
+app.get(
+  "/hello(.html)?",
+  (_req, _res, next) => {
+    console.log("attempted to load hello.html");
+    next();
+  },
+  (_, res) => {
+    res.send("Hello world");
+  }
+);
+
+const one = (_req, _res, next) => {
+  console.log("one");
+  next();
+};
+const two = (_req, _res, next) => {
+  console.log("two");
+  next();
+};
+const three = (_req, res) => {
+  console.log("three");
+  res.send("finished");
+};
+
+app.get("/chain(.html)?", [one, two, three]);
+
+app.get("/*", (_, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
